a334d02e2bada17a6d0009a53c8c898c753360a3
Michael Frericks <Michael.Frericks@f-i.de>
2014-09-24 18:02:12 +0200

    Clean up

--- src/testreagent/se2.cljs
+++ src/testreagent/se2.cljs
@@ -6,12 +6,32 @@
 
 (enable-console-print!)
 
-;; Guess number game
-
+;; event-bus
 (def event-bus (async/chan))
 (def event-bus-pub (async/pub event-bus first))
 
+;; model
 (def game-model (atom nil))
+
+;; UI
+(defn guess-number-game [chan]
+  [:div
+   "Rate eine Zahl zwischen 1 und 100 "
+   [:p]
+   "Dein Tipp:"
+   [:input {:type "number"
+            :value (:my-guess @game-model)
+            :disabled (:game-over? @game-model)
+            :on-change (fn [e] (async/put! chan [:update-guess (-> e .-target .-value int)]))}]
+   [:button {:type "button"
+             :disabled (:game-over? @game-model)
+             :on-click (fn [e] (async/put! chan [:make-guess]))}
+    "Rate!"]
+   [:button {:type "button"
+             :disabled (not (:game-over? @game-model))
+             :on-click (fn [e] (async/put! chan [:reset-game]))}
+    "Start"]
+   [:div (:message @game-model)]])
 
 
 (defn game-init []
@@ -34,65 +54,32 @@
      (> (:secret-number model) (:my-guess model)) (str (:my-guess model) " ist zu klein! Noch " (- 7 (:number-of-guesses model)) " Versuche" )
      (< (:secret-number model) (:my-guess model)) (str (:my-guess model) " ist zu groß! Noch " (- 7 (:number-of-guesses model)) " Versuche" ))))
 
-;; UI helper funcrtions
+(defn make-guess [old-model]
+  (as-> old-model x
+        (update-in x [:number-of-guesses] inc)
+        (assoc-in x [:game-over?] (game-over? x))
+        (assoc-in x [:message] (game-message x))))
 
-(defn- make-guess [chan]
-  (async/put! chan [:make-guess]))
-
-(defn- update-my-guess [chan guess]
-  (async/put! chan [:update-guess guess]))
-
-(defn- reset-game [chan]
-  (async/put! chan [:reset-game]))
-
-;; UI
-(defn guess-number-game [chan]
-  [:div
-   "Rate eine Zahl zwischen 1 und 100 "
-   [:p]
-   "Dein Tipp:"
-   [:input {:type "number"
-            :value (:my-guess @game-model)
-            :disabled (:game-over? @game-model)
-            :on-change (fn [e] (update-my-guess chan (-> e .-target .-value int)))}]
-   [:button {:type "button"
-             :disabled (:game-over? @game-model)
-             :on-click (fn [e] (make-guess chan))}
-    "Rate!"]
-   [:button {:type "button"
-             :disabled (not (:game-over? @game-model))
-             :on-click (fn [e] (reset-game chan))}
-    "Start"]
-   [:div (:message @game-model)]])
-
-
+(defn edit-my-guess [number old-model]
+  (assoc-in old-model [:my-guess] number))
 
 ;; when game is started
 (go-loop-sub event-bus-pub :reset-game [_]
              (reset! game-model (game-init)))
 
 ;; when button 'Rate' is clicked
-(go-loop-sub event-bus-pub :make-guess [_]
-             (swap! game-model
-                    (fn [old]
-                      (as-> old x
-                            (update-in x [:number-of-guesses] inc)
-                            (assoc-in x [:game-over?] (game-over? x))
-                            (assoc-in x [:message] (game-message x))))))
+(go-loop-sub event-bus-pub :make-guess [_] (swap! game-model make-guess))
 
-;; when guessed number is changed
+;; when guessed number gets edited
 (go-loop-sub event-bus-pub :update-guess [_ number]
-             (swap! game-model
-                    (fn [old]
-                      (assoc-in old [:my-guess] number))))
+             (swap! game-model (partial edit-my-guess number)))
+
+;; Start the app
 
 (defn run []
-  (reset-game event-bus)
+  (async/put! event-bus [:reset-game])
   (reagent/render-component
    [guess-number-game event-bus]
    (.-body js/document)))
 
 (run)
-
-
-
